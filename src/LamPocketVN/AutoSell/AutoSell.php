3e06bd8d85f371b3e0ce4dd19e7cf5e5b13d1e1c
Adam <adamsuper169@gmail.com>
2020-03-24 23:59:28 -0700

    Rework auto sell

--- src/LamPocketVN/AutoSell/AutoSell.php
+++ src/LamPocketVN/AutoSell/AutoSell.php
@@ -2,118 +2,94 @@
 
 namespace LamPocketVN\AutoSell;
 
-use pocketmine\{Player, Server};
-use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\command\{Command,CommandSender, CommandExecutor, ConsoleCommandSender};
-use pocketmine\inventory\BaseInventory;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\item\Item;
+use onebone\economyapi\EconomyAPI;
+use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
-use pocketmine\utils\config;
+use pocketmine\Player;
 
-class AutoSell extends PluginBase implements Listener
-{
-	public $config;
+class Main extends PluginBase implements Listener {
+
+    public $cfg;
 	
-	public $mode = [];
-	
-	public function onEnable() : void
-	{
-		$this->saveResource("setting.yml");
-		$this->config = new Config($this->getDataFolder() . "setting.yml", Config::YAML);
-		$this->set = $this->config->getAll();
-		
-                $this->getLogger()->info(TextFormat::GREEN . "Plugin enabled! [Plugin by LamPocketVN]");
-                $this->getServer()->getPluginManager()->registerEvents($this, $this);	
+	private $mode = [];
+
+    public function onEnable() : void{
+
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+        $this->cfg = new Config($this->getDataFolder() . "sell.yml" , Config::YAML);
     }
 	
-	public function onDisable() : void
+	public function onJoin (PlayerJoinEvent $j)
 	{
-		$this->getLogger()->info(TextFormat::RED . "Plugin disabled!");
+	    $player = $j->getPlayer()->getName();
+		$this->mode[$player] = "off";
 	}
 	
-	
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
-	{
-		if (!$sender instanceof Player)
-		{
-			$sender->sendMessage("Please use this command in game !");
-			return true;
-		}
-		if (strtolower($cmd->getName()) == "autosell") 
-	   {
-           if(!isset($args[0]))
-		   {
-               $sender->sendMessage($this->set["usage"]);
-               return false;
-           }
-           switch ($args[0]) 
-		   {
-			   case "on":
-			       if (!$this->isAutoSell($sender))
-				   {
-					   $sender->sendMessage($this->set["enabled"]);
-				       $this->mode[$sender->getName()] = "on";
-				   }
-			       else $sender->sendMessage($this->set["has-enabled"]);
-				   break;
-               case "off":
-			       if ($this->isAutoSell($sender))
-				   {
-					   $sender->sendMessage($this->set["disabled"]); 
-                       $this->mode[$sender->getName()] = "off";
-				   }
-			       else $sender->sendMessage($this->set["has-disabled"]);
-				   break;
-               default :
-                   $sender->sendMessage($this->set["usage"]);
-                   break;
-           }
-       }
-
-       return true;
-   }
-	
-/*============================== Event Listener ==============================*/
-	
-    /**
-     * @param BlockBreakEvent $event
-     * @priority HIGH
-     */
-	
-    public function onBreak(BlockBreakEvent $event)
-	{
-		$player = $event->getPlayer();
-		foreach($event->getDrops() as $drop) {
-			if(!$player->getInventory()->canAddItem($drop)) 
-			{
-				if ($this->isAutoSell($player))
-				{
-					$this->getServer()->dispatchCommand($player, $this->set["sellcmd"]);
-					$player->sendMessage($this->set["sell"]);
+    public function replaceVars($str, array $vars) : string{
+        foreach($vars as $key => $value){
+            $str = str_replace("{" . $key . "}", $value, $str);
+        }
+        return $str;
+    }
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+        switch($command->getName()){
+            case "autosell":
+                if(!$sender->hasPermission("autosell.command")){
+                    $sender->sendMessage(TextFormat::colorize($this->msg["error.permission"]));
+                    return true;
+                }
+				switch($args[0]){
+					case "on":
+					    $sender->sendMessage("§aAuto sell turned on!");
+						$this->mode[$sender->getName()] = "on";
+						break;
+				    case "off":
+					    $sender->sendMessage("§cAuto sell turned off!");
+						$this->mode[$sender->getName()] = "off";
+						break;
+					default:
+					    $sender->sendMessage("§cUsage:§7 /autosell <on/off>");
+						break;
 				}
-				break;
+        }
+		return true;
+    }
+	public function sell(Player $player){
+		$items = $player->getInventory()->getContents();
+		foreach($items as $item){
+			if($this->cfg->get($item->getId()) !== null && $this->cfg->get($item->getId()) > 0){
+				$price = $this->cfg->get($item->getId()) * $item->getCount();
+				EconomyAPI::getInstance()->addMoney($player, $price);
+				//$sender->sendMessage("§aSell all your contents for§b $price §c(x1)");
+				$player->getInventory()->remove($item);
+			}
+		}
+	}
+	public function onBreak(BlockBreakEvent $event) : void{
+		$player = $event->getPlayer();
+		foreach($event->getDrops() as $drop){
+			if(!$player->getInventory()->canAddItem($drop)){
+				if($this->mode[$player->getName()] == "on"){
+					$this->sell($player);
+					$player->sendMessage("§aSell all your contents");
+				}
 			}
 		}
 	}
 	
-	public function onJoin (PlayerJoinEvent $event)
-	{
-	    $player = $event->getPlayer()->getName();
-		$this->mode[$player] = "off";
-	}
-//=============================== API ========================================
-
-	public function isAutoSell(Player $player): bool
-	{
-		if ($this->mode[$player->getName()] === "on") 
-		{
-			return true;
-		}
-		else{
-			return false;
-		} 
-	}
+	public function onQuit(PlayerQuitEvent $e){
+       $a = "autosell off";
+       $this->getServer()->dispatchCommand($e->getPlayer(),$a);
+    }
+    
 }
